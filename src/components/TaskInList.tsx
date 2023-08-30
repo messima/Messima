8cb2e1b6871aa80362421798f57a9310f6e9e799
brigazvi <37640970+brigazvi@users.noreply.github.com>
2023-08-30 14:18:40 +0300

    support for adding tags to new tasks

--- src/components/TaskInList.tsx
+++ src/components/TaskInList.tsx
@@ -17,7 +17,7 @@ export default function TaskInList({
         task.done ? "bg-slate-700 " : "bg-slate-600"
       }`}
     >
-      <div className="flex items-center gap-2">
+      <div className="flex items-start gap-2">
         <span
           className="flex aspect-square w-6  cursor-pointer items-center justify-center rounded-md bg-slate-900"
           onClick={(e) => {
@@ -36,7 +36,7 @@ export default function TaskInList({
           </span>
         </span>
         <span
-          className={`cursor-default text-lg font-bold tracking-wide ${
+          className={`cursor-default text-lg font-bold leading-[1.4rem] tracking-wide ${
             task.done ? "line-through" : ""
           }`}
         >
@@ -45,7 +45,7 @@ export default function TaskInList({
       </div>
       <div className="greyedOut cursor-default">{task.description}</div>
       {task.tags !== null && (
-        <div className="mt-2 flex gap-2">
+        <div className="mt-2 flex flex-wrap gap-2">
           {task.tags.map((tag, i) => (
             <Tag tag={tag} key={i} />
           ))}
--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,10 +1,12 @@
-import { useRef, useState } from "react"
+import { Dispatch, SetStateAction, useRef, useState } from "react"
 import { Link, redirect, useNavigate } from "react-router-dom"
 import { addNewTask } from "../functions/services/task.service"
 import NavBar from "../Layout/NavBar"
 export default function NewTask() {
   const titleInputElement = useRef<HTMLInputElement>(null)
   const descriptionInputElement = useRef<HTMLInputElement>(null)
+  const [newTag, setNewTag] = useState("")
+  const [tags, setTags]: [tags: string[], setTags: any] = useState([])
   const [errMsg, setErrMsg] = useState("")
   const navigate = useNavigate()
   return (
@@ -20,7 +22,7 @@ export default function NewTask() {
               id: Date.now(),
               title: titleInputElement.current.value,
               description: descriptionInputElement.current.value,
-              tags: [""],
+              tags: tags,
               done: false,
             }).then(() => {
               navigate("/")
@@ -49,10 +51,40 @@ export default function NewTask() {
           id="description"
           ref={descriptionInputElement}
         />
-        <label htmlFor="tags">tags</label>
-        <form action="">
-          <input type="text" name="newTag" id="newTag" className="textInput" />
-        </form>
+        <div className="flex gap-2">
+          <label htmlFor="newTag">Tags</label>
+          {tags.map((tag, i) => (
+            <span
+              className="cursor-pointer rounded bg-slate-900 px-1 hover:bg-slate-950"
+              onClick={() => setTags(tags.filter((v) => v !== tag))}
+              key={i}
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className="flex gap-2">
+          <input
+            type="text"
+            name="newTag"
+            id="newTag"
+            className="textInput"
+            value={newTag}
+            onInput={(e) => setNewTag(e.currentTarget.value)}
+          />
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              if (newTag.trim() !== "") {
+                setTags([...tags, newTag])
+                setNewTag("")
+              }
+            }}
+            className="grid aspect-square h-full w-12 place-items-center  rounded bg-slate-500 hover:bg-slate-600"
+          >
+            <span className="material-symbols-outlined">add</span>
+          </button>
+        </div>
         <div>{errMsg}</div>
         <button type="submit" className="rounded-lg bg-blue-500 px-3 py-2">
           Add Task
